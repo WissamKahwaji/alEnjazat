7e954bd7b2d3b2504f7230f372f355d80cab272f
WissamKahwaji <kahwajiwisam@gmail.com>
2024-06-04 16:47:25 +0400

    ad

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import BackgroundImage from "../../components/pages/about-us/BackgroundImage";
 import AboutContentItem from "../../components/pages/about-us/AboutContentItem";
