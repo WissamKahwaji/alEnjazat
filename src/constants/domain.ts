9914dfca923fb5cdacb0fab573e6206616bda651
WissamKahwaji <kahwajiwisam@gmail.com>
2024-06-08 16:30:19 +0400

    ed

--- src/constants/domain.ts
+++ src/constants/domain.ts
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://alenjazatback.siidevelopment.com";
 // "http://localhost:5000";
 //  const baseUrl = "https://alenjazatback.siidevelopment.com/";
 export default baseUrl;
